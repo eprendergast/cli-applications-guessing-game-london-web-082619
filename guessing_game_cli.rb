f33d2890e945de12e4766b60f34b83ab1e836648
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-08 11:46:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,22 +4,19 @@ def guessing_game_cli
   random_number = rand(1..6) # Generate and store a random number between 1 and 6
   puts "Welcome! Please guess a number betweek 1-6:" # Prompts the user to guess their own number between 1 and 6
   user_input = gets.chomp # Capture user input from the command line
-  if user_input == random_number
+  # Compare that input to the random number that has been generated
+  # Print out one of three statements:
+  if user_input == random_number # If the user's input matches the random number: You guessed the correct number!
     puts "You guessed the correct number!"
-  elsif user_input == "exit"
-    puts "Goodbye!"
-  else
+  elsif user_input == "exit"  # It the user's input is equal to "exit": Goodbye!
+    puts "Goodbye!"  
+  else # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
     puts "Sorry! The computer guessed #{random_number}."
   end
 
 end
 #
-guessing_game_cli
 
 
 
-# Compare that input to the random number that has been generated
-# Print out one of three statements:
-    # If the user's input matches the random number: You guessed the correct number!
-    # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
-    # It the user's input is equal to "exit": Goodbye!
+
