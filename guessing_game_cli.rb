5144827e113a5478504ba7e7f29ce4e1277218ed
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-08 11:54:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,18 +1,27 @@
-# Code your solution here!
+
+
+# Capture user input from the command line
+# Compare that input to the random number that has been generated
+# Print out one of three statements:
+    # If the user's input matches the random number: You guessed the correct number!
+    # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
+    # It the user's input is equal to "exit": Goodbye!
+
+# Generate and store a random number between 1 and 6
+def generate_number
+  random_number = rand(1..6)
+end
+
+# Prompts the user to guess their own number between 1 and 6
+def prompt_user
+  puts "Please enter a number between 1-6"
+end
+
+def get_input
+  user_input = gets.chomp
+end
 
 def run_guessing_game
-  random_number = rand(1..6) # Generate and store a random number between 1 and 6
-  puts "Welcome! Please guess a number betweek 1-6:" # Prompts the user to guess their own number between 1 and 6
-  user_input = gets.chomp # Capture user input from the command line
-  # Compare that input to the random number that has been generated
-  # Print out one of three statements:
-  if user_input == random_number # If the user's input matches the random number: You guessed the correct number!
-    puts "You guessed the correct number!"
-  elsif user_input == "exit"  # It the user's input is equal to "exit": Goodbye!
-    puts "Goodbye!"
-  else # If the user's input DOES NOT matches the random number: Sorry! The computer guessed <number>.
-    puts "Sorry! The computer guessed #{random_number}."
-  end
 
 end
 #
